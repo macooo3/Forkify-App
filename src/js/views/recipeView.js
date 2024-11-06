c68eb4d87e8bd5e2ca92ab34447bdb6437139369
macooo3 <marcoysu765@gmail.com>
2024-11-05 20:06:26 -0500

    fixed bookmark click

--- src/js/views/recipeView.js
+++ src/js/views/recipeView.js
@@ -27,6 +27,7 @@ class RecipeView extends View {
   addHandlerAddBookmark(handler) {
     this._parentElement.addEventListener('click', function (e) {
       const btn = e.target.closest('.btn--bookmark');
+      if(!btn) return
       handler();
     });
   }
